cc63bd329fdb6af97e2dd55f909aeed8f73ba207
AiJiangnan <jnai@gaojihealth.com>
2022-05-16 14:50:52 +0800

    初始化仓库

---
+++ src/main/java/cn/codeartist/mybatis/Main.java
@@ -0,0 +1,14 @@
+package cn.codeartist.mybatis;
+
+/**
+ * MyBatis
+ *
+ * @author 艾江南
+ * @date 2022/5/16
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Main.main");
+    }
+}
